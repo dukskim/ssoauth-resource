db2188aa7f5daaf074fd771807d8148ef19786ba
dh.kim <dh.kim@cmsedu.co.kr>
2020-03-23 15:27:32 +0900

    init project

---
+++ src/main/java/com/ssoauth/SsoauthResourceApplication.java
@@ -0,0 +1,13 @@
+package com.ssoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoauthResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoauthResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssoauth/SsoauthResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoauthResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
